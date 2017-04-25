b756748858b62cdd70279a47c057b6e8ff773c8f
Phantasy-Worlds <Phantasy-Worlds@users.noreply.github.com>
2017-04-25 09:21:57 +0200

    [LuxEngine]: Added missing file

---
+++ src/video/Material.cpp
@@ -0,0 +1,14 @@
+// Local includes
+#include "video/Material.h"
+
+namespace lux 
+{
+namespace video 
+{
+
+Material IdentityMaterial;
+Material WorkMaterial;
+
+} // namespace lux
+} // namespace video
+
