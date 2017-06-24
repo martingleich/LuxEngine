eab5a373a17ee6078956de2375fa12f806a167dc
martingleich <martin.gleich3@web.de>
2017-06-24 09:29:46 +0200

    Improved handling of .def files. Should now work for most visual studio versions. All temporary files are now located in the cmake build directory.

--- scripts/GenerateDef.bat
+++ scripts/GenerateDef.bat
@@ -1,3 +1,17 @@
-echo LIBRARY LuxEngine > "%~4"
-echo EXPORTS >> "%~4"
-for %%f in (%~2\*.lib) do "%VS140COMNTOOLS%..\..\VC\bin\dumpbin.exe" /LINKERMEMBER:1 "%%f" | "%~1" "%~3" >> "%~4"
+REM 1: MSCV_PATH 2: FilterExportFiles 3: library dir 4: export.txt 5: output file
+
+echo LIBRARY LuxEngine > "%~5"
+echo EXPORTS >> "%~5"
+
+if exist "%~1\dumpbin.exe" (
+	set DUMP_BIN="%~1\dumpbin.exe" /LINKERMEMBER:1
+) else if exist "%~1\link.exe" (
+	set DUMP_BIN="%~1\link.exe" /DUMP /LINKERMEMBER:1
+) else if exist "dumpbin.exe" (
+	set DUMP_BIN=dumpbin.exe /LINKERMEMBER:1
+) else (
+	echo Missing dumpbin, can't create def files.
+	exit /b 1
+)
+
+for %%f in (%~3\*.lib) do %DUMP_BIN% "%%f" | "%~2" "%~4" >> "%~5"
--- scripts/ValidateDependencies.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-if not exist "%~dp0\..\tmp" mkdir "%~dp0\..\tmp"
-
-if not exist "%VS140COMNTOOLS%..\..\VC\bin\dumpbin.exe" (
-	echo Missing dumpbin.exe, Visual Studio 2015 is missing or corrupted.
-	echo Search path is: "%VS140COMNTOOLS%..\..\VC\bin\dumpbin.exe"
-	exit /b 1
-)
