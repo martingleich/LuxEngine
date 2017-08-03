eacfb3c89bb01036badce1b82224c729103cb9a4
martingleich <martin.gleich3@web.de>
2017-08-03 22:07:16 +0200

    Added background image to materialtest.

--- testing/MaterialTest/src/main.cpp
+++ testing/MaterialTest/src/main.cpp
@@ -74,8 +74,31 @@ public:
 		node = Context.Smgr->AddNode(m_Light);
 		node->SetDirection(math::Vector3F::UNIT_Z);
 
+		m_CheckerTexture = Context.ImgSys->CreateFittingTexture(math::Dimension2U(32, 32));
+		m_CheckerTexture->SetFiltering(video::BaseTexture::Filter::Point);
+		{
+			auto canvas = std::move(m_CheckerTexture->GetCanvas(video::BaseTexture::ELockMode::Overwrite));
+			bool black = false;
+			core::Randomizer rand(123456);
+			for(size_t r = 0; r < canvas.GetHeight(); ++r) {
+				for(size_t c = 0; c < canvas.GetWidth(); ++c) {
+					black = ((r + c) % 2 == 0);
+					if(rand.GetBool(0.1f)) {
+						canvas.SetPixel(c, r, video::HSVToColorf(rand.GetFloat(0,1), 1.0f, 1.0f).ToHex());
+					} else {
+						if(black)
+							canvas.SetPixel(c, r, video::Color::Black);
+						else
+							canvas.SetPixel(c, r, video::Color::White);
+					}
+				}
+			}
+		}
+
 		node = Context.Smgr->AddSkyBox();
-		node->GetComponent<scene::SkyBox>()->GetMaterial(0)->SetDiffuse(video::Color::Blue);
+		auto skyBox = node->GetComponent<scene::SkyBox>();
+		skyBox->UseCubeTexture(false);
+		skyBox->SetSkyTexture(m_CheckerTexture);
 	}
 
 	void Run()
@@ -101,6 +124,8 @@ private:
 
 	StrongRef<gui::Font> m_Font;
 
+	StrongRef<video::Texture> m_CheckerTexture;
+
 	float m_Time = 0.0f;
 };
 
@@ -117,9 +142,15 @@ void MaterialTest::Render()
 core::Array<StrongRef<video::Material>> MaterialTest::GenMaterialList()
 {
 	core::Array<StrongRef<video::Material>> out;
+	StrongRef<video::Material> mat;
 
-	auto mat = Context.MatLib->CreateMaterial("solid");
+	mat = Context.MatLib->CreateMaterial("solid");
 	mat->SetDiffuse(video::Color::Red);
+	out.PushBack(mat);
+
+	mat = Context.MatLib->CreateMaterial("transparent");
+	mat->SetDiffuse(video::Color::Green);
+	mat->SetAlpha(0.5f);
 	out.PushBack(mat);
 
 	return out;
@@ -154,6 +185,12 @@ void MaterialTest::Load()
 			startPoint.y + row*delta,
 			0.0f);
 		node->SetPosition(pos);
+
+		col += 1;
+		if(col == 6) {
+			++row;
+			col = 0;
+		}
 	}
 }
 
